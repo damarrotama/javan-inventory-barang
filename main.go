b39dea09967807a3aa2153f87aa5a346ebd51847
Armada Muhammad <armada.muhammad@hyperscal.com>
2026-03-30 15:19:34 +0700

    define router

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"javan-inventory-barang/routes"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	app := fiber.New()
+
+	routes.Handle(app)
+	if err := app.Listen(":" + viper.GetString("PORT")); err != nil {
+		log.Fatal(err)
+	}
+}
